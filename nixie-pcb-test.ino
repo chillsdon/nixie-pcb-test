71933e175f6b3ce4ae9e73e6c48e428857720c70
Craig Hillsdon <craigh@itecsystems.com>
2020-10-07 05:53:29 +0100

    Trying to get the PCB working with a HV transistor used to disable the nixie tube when I do not require it to be on.

--- nixie-pcb-test.ino
+++ nixie-pcb-test.ino
@@ -2,6 +2,7 @@ const int pinA = 2;
 const int pinB = 3;
 const int pinC = 4;
 const int pinD = 5;
+const int pinEN = 8;
 
 int valueA;
 int valueB;
@@ -9,12 +10,19 @@ int valueC;
 int valueD;
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   pinMode(pinA, OUTPUT);
   pinMode(pinB, OUTPUT);
   pinMode(pinC, OUTPUT);
   pinMode(pinD, OUTPUT);
+  pinMode(pinEN, OUTPUT);
+
+  //Nixie anode is fed from 170V power supply via a
+  //MSPA42 high voltage transistor whose BASE is
+  //connected to pinEN on the MCU making this an
+  //Enable pin.
+  digitalWrite(pinEN, HIGH);
 }
 
 void loop() {
@@ -31,13 +39,19 @@ void loop() {
     digitalWrite(pinC, valueC);
     digitalWrite(pinD, valueD);
 
+    //digitalWrite(pinEN, HIGH);
+
+    /*
     Serial.print(i);
     Serial.print(" = ");
     Serial.print(valueD);
     Serial.print(valueC);
     Serial.print(valueB);
     Serial.println(valueA);
+    */
     
     delay(500);
+    //digitalWrite(pinEN, LOW);
+    //delay(500);
   }
 }
